45ef3b42868d4f28d86f2cc01ba2915f24a82ea0
Shiying Lai <sl4849@columbia.edu>
2020-11-14 03:40:42 -0500

    Question 3 Add Tests

--- tests/test_cipher_sl4849.py
+++ tests/test_cipher_sl4849.py
@@ -1,5 +1,7 @@
-from cipher_sl4849 import __version__
 from cipher_sl4849 import cipher_sl4849
 
-def test_version():
-    assert __version__ == '0.1.0'
+def test_cipher_single_word():
+    example ='apple'
+    expected ="dssoh"
+    actual = cipher_sl4849.cipher(text=example, shift=3, encrypt=True)
+    assert actual == expected
